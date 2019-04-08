0a6ed77503b313acd3dcc761f91e491210f93645
3332063 <49257968+3332063@users.noreply.github.com>
2019-04-09 09:21:37 +1000

    added dictionary search

--- vowel_searcher.c
+++ vowel_searcher.c
@@ -1,35 +1,39 @@
 #include <stdio.h>
 #include <string.h>
 
-int main()
+int main(int argc, char const *argv[])
 {
-  char string[100];
+  int num =0;
+  char word[100] = {0};
+	FILE *in_file = fopen("dictionary.txt", "r");
+  if (in_file == NULL)
+    {
+      printf("Error file missing\n");
+      return 0;
+    }
+  char string[50];
   printf("Enter a word: ");
-  scanf("%99s", string);
-  char c=string[1];
-  printf("The second letter in %s is %c\n", string, c);
-  char vowels=
-  if (string[1]=='a')
+  scanf("%99s", word);
+  char c=word[1];
+  printf("The second letter in %s is %c\n", word, c);
+  if (word[1]=='a'||word[1]=='A'||word[1]=='e'||word[1]=='E'||word[1]=='i'||word[1]=='I'||word[1]=='o'||word[1]=='O'||word[1]=='u'||word[1]=='U')
   {
-    printf("The letter 'a' is a vowel\n");
+    printf("The letter '%c' is a vowel\n", c);
   }
-  else if (string[1]=='e')
-  {
-    printf("The letter 'e' is a vowel\n");
-  }
-  else if (string[1]=='i')
-  {
-    printf("The letter 'i' is a vowel\n");
-  }
-  else if (string[1]=='o')
-  {
-    printf("The letter 'o' is a vowel\n");
-  }
-  else if (string[1]=='u')
-  {
-    printf("The letter 'u' is a vowel\n");
-  }
-  else
-  printf("The second letter is not a vowel\n");
+  while ( fscanf(in_file,"%s", string) == 1)
+    {
+      if(strstr(string, word)!=0)
+				{
+          num++;
+        }
+    }
+  printf("The word %s was found in the file dictionary.txt %d times\n",word,num );
+  num = 0;
+  fclose(in_file);
+  //int i;
+  //for(i=0; i<='z'; i++)
+  //{
+
+  //}
   return 0;
 }
