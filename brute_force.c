a12f190333513a14501f8b24e779c89dc52ad0a2
3332063 <49257968+3332063@users.noreply.github.com>
2019-04-09 23:10:36 +1000

    updated brute force attack

--- brute_force.c
+++ brute_force.c
@@ -2,17 +2,16 @@
 #include <stdlib.h>
 #include <string.h>
 
-char word[40]="Mitchell";
+char word[40]="qmxgl";
 
 int main()
 {
   int n;
-  for(n=0; n<strlen(word); n++)
+  for (int kv=1; kv<25; kv++)
     {
-      for (int kv=1; kv<25; kv++)
+      for(n=0; n<strlen(word); n++)
         {
-          word[n]=word[n]+kv;
-          kv++;
+          word[n]=word[n]-kv;
         }
       printf("%s\n", word);
     }
