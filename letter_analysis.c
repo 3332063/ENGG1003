efa4fcff01d7a767ca6ab3457ad8e45571b3c6e4
3332063 <49257968+3332063@users.noreply.github.com>
2019-04-17 14:52:06 +1000

    Letter analysis software

---
+++ letter_analysis.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+   char string[1024];
+   int c = 0, count[26] = {0}, x;
+
+   printf("Enter a string\n");
+   scanf("%[^\n]s", string);
+
+   while (string[c] != '\0') {
+   /** Considering characters from 'a' to 'z' only and ignoring others. */
+
+      if (string[c] >= 'a' && string[c] <= 'z') {
+         x = string[c] - 'a';
+         count[x]++;
+      }
+      if(string[c]>='A' && string[c]<='Z')
+        {
+          x=string[c]-'A';
+          count[x]++;
+        }
+
+      c++;
+   }
+
+   for (c = 0; c < 26; c++)
+         printf("%c occurs %d times in the string.\n", c + 'A', count[c]);
+
+   return 0;
+}
